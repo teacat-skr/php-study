00a4284ed108e130f0768787081756fead96d0cd
teacat <railcore03@gmail.com>
2019-05-07 05:42:07 +0900

    update

--- phpprac/part5.php
+++ phpprac/part5.php
@@ -13,12 +13,61 @@ var_dump(isset($obj->{'1'}));
 var_dump(key($obj));
 //string(1) "1"になる。資料が間違えてる?
 $obj = (object) 'ciao';
-echo $obj->scalar;
+echo $obj->scalar.PHP_EOL;
 //scalarに格納
 //リソース型はファイル、データベース接続などの特別なハンドルを保持するため変換不可
 //データベース接続以外は参照されなくなると勝手にGCが削除してくれる
 $var = NULL;
 //NULL型、大文字小文字の区別なし(nullでも可)
 //NULLへのキャストは非推奨
+?>
+<?php
+function my_call_function(){//コールバック関数
+    echo 'hello world!'.PHP_EOL;
+}
 
+class MyClass{
+    static function myCallbackMethod(){//コールバックメソッド
+        echo 'Hello World!'.PHP_EOL;
+    }
+}
+call_user_func('my_call_function');
+//単純なコールバック
+call_user_func(array('MyClass','myCallbackMethod'));
+//静的クラスメソッドのコール1
+$obj = new MyClass();
+call_user_func(array($obj, 'myCallbackMethod'));
+//オブジェクトメソッドのコール
+call_user_func('MyClass::myCallbackMethod');
+//静的クラスメソッドのコール2
+class A {
+    public static function who() {
+        echo "A\n";
+    }
+}
+class B extends A {
+    public static function who() {
+        echo "B\n";
+    }
+}
+call_user_func(array('B', 'parent::who'));//A
+//相対指定による静的クラスメソッドのコール
+class C {
+    public function __invoke ($name) {
+        echo 'Hello ', $name, "\n";
+    }
+}
+$c = new C();
+call_user_func($c, 'PHP!');
+//以上見た通りに使える
 
+//クロージャ=無名関数
+$double = function ($a) {
+    return $a * 2;
+};
+
+$numbers = range(1, 5);
+//pythonで見た,(x: 1<=x<=5)
+$new_numbers = array_map($double, $numbers);
+print implode(' ', $new_numbers);
+//この関数C++に欲しい
