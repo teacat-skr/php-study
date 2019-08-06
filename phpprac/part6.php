8ea7e94731c39335d79369c3acc68ed8164e4a8d
teacat <railcore03@gmail.com>
2019-08-07 01:24:44 +0900

    update

--- phpprac/part6.php
+++ phpprac/part6.php
@@ -4,7 +4,7 @@ $foo = 'bob';
 $bar = &$foo;
 //参照、C言語等と同様
 //右辺値は参照できない
-//変数を初期化することは良いこと
+//変数を初期化することは良いことである
 $unset_obj -> foo = 'bar';
 var_dump($unset_obj);
 //objectの初期化はwarningが出る?
@@ -35,10 +35,47 @@ function sum2 () {
     $GLOBALS['b'] = $GLOBALS['a'] + $GLOBALS['b'];
 }
 sum2();
-echo $b;
+echo $b,PHP_EOL;
 //同じ結果になる
 //$GLOBALSは連想配列でありグローバル変数名がキー、内容が配列要素の値となっている
-
 ?>
+<?php
+function test1 () {
+    $a = 0;
+    echo $a;
+    $a++;
+}
+//この場合関数が呼び出されるごとに$aが宣言され解体されるため$a++は無駄である
+function test2 () {
+    static $a = 0;
+    echo $a;
+    $a++;
+}
+//staticで静的な宣言すると一度目だけ初期化され、以降解体されずインクリメントされ続けられる
+//静的な宣言は関数を用いた宣言はできない ex)static $int = sqrt(121)
+//リファレンスは静的に保存できない
+?>
+<?php
+$a = 'hello';
+$$a = 'world';
+//変数を可変にすることができる
+echo "$a ${$a}",PHP_EOL;
+echo "$a $hello",PHP_EOL;
+//同じ結果になる
+//$$a[1]とかくと${$a[1]}なのか${$a}[1]なのかを区別して解釈させなければならない
+//クラスのプロパティ名も同様に可変プロパティ名でアクセスできる
+class foo{
+    var $bar = 'I am bar';
+}
+$foo = new foo();
+$start = 'b';
+$end = 'ar';
+echo $foo ->{$start. $end};
+//プロパティ名をこのようにも記述できる
+//echo $_POST['username'];
+//echo $_REQUEST['username'];
+//HTMLホームからデータにアクセスする方法は上位の二つのみ
+//PHP5.4.0以前のバージョンではほかにもあった
 
+//PHPは送られた変数名にピリオド(.)があった場合アンダースコア(_)に自動で変換する
 
