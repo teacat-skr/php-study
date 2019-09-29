74b4bb4abe87098d3fb6e5dae6343f0af02e9cfc
teacat <railcore03@gmail.com>
2019-09-30 01:43:01 +0900

    update

--- phpprac/part10.php
+++ phpprac/part10.php
@@ -1,1 +1,6 @@
 <?php
+echo preg_replace_callback('~-([a-z])~', function ($match){
+    return strtoupper($match[1]);
+}, 'hello-world');
+//正規表現'~-([a-z])~'は-とa-zのどれか1文字を表していて検索された-wの2文字目をWにしてその文字だけ返却されている
+//無名関数は名前を付けずに変数に代入して使える使えるため一度しか使わないような関数を手軽に作れる
